37f50d659a3637bf695fbc0c45c09ebea93f61ef
Ali KhoshKholgh <45786220+Alikhoshkholgh@users.noreply.github.com>
2022-11-22 02:49:40 +0330

    Create tcp-port-check-multipleIP.ps1

---
+++ Tools/tcp-port-check-multipleIP.ps1
@@ -0,0 +1,20 @@
+$addressList = "192.168.11.1", "192.168.11.2", "192.168.11.3", "192.168.11.4"
+$portList = 3389,135,445 
+$hostname = $env:computername
+$delay_perReq = 10
+
+write-output "scanning initiated by $hostname"
+
+
+foreach($ip in $addressList){
+    
+    foreach($port in $portList){
+
+        $conn_res=(test-netconnection -computername $ip -port $port -warningAction silentlycontinue)
+        $comName = $conn_res.tcptestsucceeded        
+        
+        if($comName){           write-output "($ip):($port)   is available for $hostname"            }
+        else{                   write-output "($ip):($port)   is not-available for $hostname"            }
+        start-sleep -Seconds $delay_perReq
+    }    
+}
