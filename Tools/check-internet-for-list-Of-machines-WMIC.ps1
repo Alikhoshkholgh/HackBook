6d09181ede884f2a4e332e474d1708b9d24a48c3
Ali KhoshKholgh <45786220+Alikhoshkholgh@users.noreply.github.com>
2022-11-22 03:51:00 +0330

    Update check-internet-for-list-Of-machines-WMIC.ps1

--- Tools/check-internet-for-list-Of-machines-WMIC.ps1
+++ Tools/check-internet-for-list-Of-machines-WMIC.ps1
@@ -1,12 +1,11 @@
 #obviously you should see the Port:135 on the target machine
-$hostname = $env:computername
 $addressList = "hostname1","hostname2","hostname3",
 $delay_perReq = 1
-$storeFile = "\\Hostname\C$\Users\Public\Documents\$hostname-checkInternet.txt"
+$storageHostname = "localhost"
 
 foreach($ip in $addressList){    
-    wmic.exe /node:$ip process call create "powershell.exe -ep bypass -c (test-netconnection google.com).pingsucceeded >> $storeFile"
-    Start-Sleep -Seconds $delay_perReq
+wmic.exe /node:$ip process call create "powershell.exe -ep bypass -c (test-netconnection google.com).pingsucceeded >> \\$storageHostname\C$\Users\Public\Documents\$ip-checkInternet.txt"
+Start-Sleep -Seconds $delay_perReq
 }
 
 
