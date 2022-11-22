ae17b8c8dd8b70157a061706dbd7f198032d1b05
Ali KhoshKholgh <45786220+Alikhoshkholgh@users.noreply.github.com>
2022-11-22 03:40:59 +0330

    Create check-internet-for-list-Of-machines-WMIC.ps1

---
+++ Tools/check-internet-for-list-Of-machines-WMIC.ps1
@@ -0,0 +1,11 @@
+$hostname = $env:computername
+$addressList = "hostname1","hostname2","hostname3",
+$delay_perReq = 1
+$storeFile = "\\Hostname\C$\Users\Public\Documents\$hostname-checkInternet.txt"
+
+foreach($ip in $addressList){    
+    wmic.exe /node:$ip process call create "powershell.exe -ep bypass -c (test-netconnection google.com).pingsucceeded >> $storeFile"
+    Start-Sleep -Seconds $delay_perReq
+}
+
+
